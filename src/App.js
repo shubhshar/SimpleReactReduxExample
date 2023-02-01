d0c04f38a5a5a8b7a5da7be79ab742ae8a325efd
Shubham Sharma <shubhr0813@gmail.com>
2023-02-01 16:34:49 +0530

    decrement using function

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import { increment, decrement, multiply } from "./actions/action";
 export default function App() {
   const count = useSelector((state) => state.counterReducer);
   const mult = useSelector((state) => state.multiplyReducer);
-
   const disp = useDispatch();
+  const dec = () => {
+    disp(decrement());
+  };
+
   return (
     <div className="App">
       <h2>Counter: {count}</h2>
       <button onClick={() => disp(increment())}>+</button>
-      <button onClick={() => disp(decrement())}>-</button>
+      <button onClick={dec}>-</button>
       <h2>
         multiply: {mult} x 3= {mult * 3}
       </h2>
