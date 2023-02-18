3cd540b7da534f4f6ab84dc28e10aaba73da0a21
Shubham Sharma <shubhr0813@gmail.com>
2023-02-18 23:25:01 +0530

    Removed resetm from counterReduc

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -6,8 +6,6 @@ const counterReducer = (initstate = 1, action) => {
       return initstate - 1;
     case "RESETC":
       return (initstate = 0);
-    case "RESETM":
-      return (initstate = 0);
     default:
       return initstate;
   }
